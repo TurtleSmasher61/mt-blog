afae2d31cabfc655b7724c0e59c6d0cb39c6d1e7
Turtle <zachariahdawsonart@gmail.com>
2018-12-29 21:03:06 -0800

    started blog

---
+++ app.js
@@ -0,0 +1,13 @@
+// Setup
+var express = require('express');
+var app = express();
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.get("/", (req, res) => {
+    res.render('index');
+ });
+
+// Listen
+app.listen(3000, () => {
+    console.log('Server listing on 3000');
+})
